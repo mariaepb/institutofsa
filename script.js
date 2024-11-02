2d60f329318870f1562ffa2786ddf2a810f59258
Maria Eduarda <mariaepb0301@gmail.com>
2024-11-02 19:43:09 +0000

    commit1

---
+++ script.js
@@ -0,0 +1,9 @@
+function showAgenda() {
+    document.getElementById('welcomePage').style.display = 'none';
+    document.getElementById('agendaPage').style.display = 'block';
+}
+
+function showWelcome() {
+    document.getElementById('agendaPage').style.display = 'none';
+    document.getElementById('welcomePage').style.display = 'block';
+}
